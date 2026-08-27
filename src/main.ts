743cb49aa84c759315efaaf71213ba5248e58dfb
olgamagic3377-jpg <olgamagic3377@gmail.com>
2026-08-26 21:37:05 -0300

    Add Streamer Hub application

---
+++ src/main.ts
@@ -0,0 +1,8 @@
+import { createApp } from 'vue'
+import './style.css'
+import App from './App.vue'
+import router from './router'
+
+createApp(App).use(router).mount('#app')
+if ('serviceWorker' in navigator) window.addEventListener('load', () => navigator.serviceWorker.register('/sw.js'))
+
